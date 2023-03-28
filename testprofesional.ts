72179151adb9f07aadff0e164ab21da880ec56cb
sergio Arroyo <chupi1987@hotmail.com>
2023-03-28 12:27:23 +0200

    clase professional

---
+++ testprofesional.ts
@@ -0,0 +1,7 @@
+import { profesional } from "./profesional";
+let a=new profesional("sergio Arroyo",35,100,1.65,true,"Inglaterra",3,"pirata");
+let b=new profesional("Irene rubio",31,90,1.70,false,"España",0,"actor");
+let c=new profesional("juan pepe",49,105,1.65,true,"España",3,"actor");
+a.mostrar();
+b.mostrar();
+c.mostrar();
